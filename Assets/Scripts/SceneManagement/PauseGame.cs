f6332bd5afce0598d3433405b091f694c3049978
Rhys <1022076@student.sae.edu.au>
2021-10-08 12:13:02 +0800

    Pause Window
    Beginning pause window & functionality

---
+++ Assets/Scripts/SceneManagement/PauseGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour
+{
+    public Image pauseWindow;
+    
+    private void OnPause()
+    {
+        
+    }
+}
